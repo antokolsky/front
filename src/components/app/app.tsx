199743b11e227bcb772e4ccb6d3625a089000355
mitrich <4irtim@gmail.com>
2024-05-07 14:32:36 +0700

    add: Базовая модель локализации интерфейса

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,36 @@
+import "@utils/localization/i18next";
+import { useTranslation } from "react-i18next";
+import "@utils/localization/i18next.ts";
+
+const languages = {
+	en: "English",
+	ru: "Russian",
+};
+
 const App = () => {
-	return <div></div>;
+	const { t, i18n } = useTranslation();
+
+	return (
+		<div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+			<h1>{t("textHello")}</h1>
+			<div style={{ display: "flex", gap: "10px" }}>
+				{Object.entries(languages).map(([lng, name]) => (
+					<button
+						key={lng}
+						onClick={() => i18n.changeLanguage(lng)}
+						disabled={i18n.language === lng}
+					>
+						{name}
+					</button>
+				))}
+			</div>
+			<p>{t("text")}</p>
+			<div style={{ display: "flex", gap: "10px" }}>
+				<button>{t("buttonOk")}</button>
+				<button>{t("buttonCancel")}</button>
+			</div>
+		</div>
+	);
 };
 
 export default App;
