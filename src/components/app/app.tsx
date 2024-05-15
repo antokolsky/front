1db06be09b1cf29cfe197d7a236695b04775075b
mitrich <4irtim@gmail.com>
2024-05-15 15:52:11 +0700

    add: стили для ресайза canvas

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -14,8 +14,13 @@ const App = () => {
 						justifyContent: "center",
 					}}
 				>
-					<div style={{ width: "1000px" }}>
-						<Model link="./cube.stl" />
+					<div style={{ maxWidth: "1000px", width: '100%', height: '100%', display: 'flex'}}>
+						<div style={{ width: "50%" }}>
+							<p>Model is on the right</p>
+						</div>
+						<div style={{ maxHeight: "500px", width: "50%", height: '50%' }}>
+							<Model link="./cube.stl" />
+						</div>
 					</div>
 				</Content>
 				<Footer></Footer>
