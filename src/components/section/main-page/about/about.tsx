9deafa636f96ee8543213b8d85063298bfaad16a
Kostya Buzunov <buzunov.constantine@yandex.ru>
2024-07-07 09:42:34 +0300

    add: Дополнить верстку

--- src/components/section/main-page/about/about.tsx
+++ src/components/section/main-page/about/about.tsx
@@ -2,13 +2,16 @@ import style from "./about.module.scss";
 
 export const About = () => {
 	return (
-		<div className={style.about}>
-			<div className={style.content}>
-				<div className={style.content__info}>
-					<h1 className={style.title}>antokolsky</h1>
-					<p className={style.subtitle}>
-						продвижение творчества российских скульпторов за рубежом
-					</p>
+		<div className={style.darkWrapper}>
+			<div className={style.about}>
+				<div className={style.content}>
+					<div className={style.content__info}>
+						<h1 className={style.title}>antokolsky</h1>
+						<p className={style.subtitle}>
+							продвижение творчества российских скульпторов за
+							рубежом
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -2,9 +2,9 @@ import About from "@/components/section/main-page/about/about";
 
 const MainPage = () => {
 	return (
-		<div>
+		<>
 			<About />
-		</div>
+		</>
 	);
 };
 
